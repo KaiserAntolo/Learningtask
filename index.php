cbaa358aee635ec3c7872d5c87ac434ab8d6a3b1
KaiserAntolo <100089862+KaiserAntolo@users.noreply.github.com>
2022-02-21 02:35:12 +0800

    Update and rename Index.php to index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body>
   <h1> wazz up you guys and welcome back to my recipe website!!! I am Kaiser Antolo </h1>
   <ul>
-    <li><a href="Index.php" target="_blank"> HOME </a></li>
+    <li><a href="index.php" target="_blank"> HOME </a></li>
     <li><a href="recipe.html" target= "_blank"> LEARNING TASK 2 </a> </li>
   </ul>
   </body>
