313cb9beedfbb9fe0fbbf5af22b602536e478333
KaiserAntolo <100089862+KaiserAntolo@users.noreply.github.com>
2022-02-21 02:25:42 +0800

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> wazz up you guys and welcome back to my recipe website!!! I am Kaiser Antolo </h1>
+  <ul>
+    <li><a href="Index.php" target="_blank"> HOME </a></li>
+    <li><a href="recipe.html" target= "_blank"> LEARNING TASK 2 </a> </li>
+  </ul>
+  </body>
+</html>
