7c585128391ffcb3e72bd06ef9a6cd5cbf6a85dc
KaiserAntolo <100089862+KaiserAntolo@users.noreply.github.com>
2022-03-24 22:54:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <li><a href="Homepage.html" target="_blank"> HOME </a></li>
     <li><a href="recipe.html" target= "_blank"> LEARNING TASK 2 </a> </li>
     <li><a href="Top10.html" target= "_blank"> LEARNING TASK 3 </a> </li>
+    <li><a href="CLE1MT.html" target= "_blank"> MIDTERM </a> </li>
   </ul>
   </body>
 </html>
